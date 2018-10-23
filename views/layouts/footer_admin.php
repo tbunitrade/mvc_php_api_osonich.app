20101842f5167651abc2841c852d3dcf0c1359f0
tbunitrade <tbunitrade@gmail.com>
2018-10-23 17:03:14 +0300

    fix postcontroller b77

--- views/layouts/footer_admin.php
+++ views/layouts/footer_admin.php
@@ -1,4 +1,4 @@
 <footer>
-    <script src="template/js/vendor.js"></script>
-    <script src="template/js/dist.js"></script>
+    <script src="./template/js/vendor.js"></script>
+    <script src="./template/js/dist.js"></script>
 </footer>
